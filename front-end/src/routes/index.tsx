d4e1de8cc742a0b2052b2ea20a3458a5dc4d137a
Rodrigo Nagoshi <rnagoshi@ciandt.com>
2021-01-24 21:49:42 -0300

    Config github pages.

--- front-end/src/routes/index.tsx
+++ front-end/src/routes/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, HashRouter } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import Report from '../pages/SongReport';
 
 const Routes: React.FC = () => {
   return (
-    <Switch>
-      <Route path="/" exact component={Dashboard} />
-      <Route path="/songsReport" component={Report} />
-    </Switch>
+    <HashRouter basename="/">
+      <Switch>
+        <Route path="/" exact component={Dashboard} />
+        <Route path="/songsReport" component={Report} />
+      </Switch>
+    </HashRouter>
   );
 };
 
